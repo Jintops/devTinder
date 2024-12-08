ed1a9e2bfc6cb685f45c56f298754588b0c7342d
jintops2003@gmail.com <jithinchackopayyanat@gmail.com>
2024-12-08 14:01:22 +0530

    exploring get post delete

--- src/app.js
+++ src/app.js
@@ -2,18 +2,33 @@ const express = require('express');
 
 const app = express();
 
-app.use("/aaa",(req,res)=>{
-    res.send("main")
-})
+
+
+
+app.get("/user",(req,res)=>{
+    res.send({name:"jinto",place:"mandalam"})
+});
+
+app.post("/user",(req,res)=>{
+    res.send("succefully added to DB")
+});
+
+app.delete("/user",(req,res)=>{
+    res.send("deleted")
+});
+
+app.patch("/user",(req,res)=>{
+    res.send("updated the user info")
+});
+app.use("/user",(req,res)=>{
+    res.send('jkhomepage')
+});
 
 app.use("/aa",(req,res)=>{
     res.send('jkhomepage')
 });
 
-app.use("/",(req,res)=>{
-    res.send("working")
-})
-console.log('lsdkfls')
+
 app.listen(3000,()=>{
     console.log('successfully connected to port')
 });
