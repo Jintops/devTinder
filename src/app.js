4f5aba4d23eb4a41c689f5fd9907cc2bee778356
jintops2003@gmail.com <jithinchackopayyanat@gmail.com>
2024-12-08 13:11:29 +0530

    created server using express

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+app.use("/a",(req,res)=>{
+    res.send("main")
+})
+
+app.use("/aa",(req,res)=>{
+    res.send('jkhomepage')
+});
+
+app.use("/",(req,res)=>{
+    res.send("working")
+})
+console.log('lsdkfls')
+app.listen(3000,()=>{
+    console.log('successfully connected to port')
+});
