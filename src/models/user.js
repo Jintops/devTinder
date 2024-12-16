f1a4255a43c73ea725decb53251d59b77484e50b
Jintops <jintops2003@gmail.com>
2024-12-16 23:01:18 +0530

    userschema methods

--- src/models/user.js
+++ src/models/user.js
@@ -68,7 +68,7 @@ userSchema.methods.getJWT = async function () {
    return token;
 }
 
-userSchema.methods.validatePassword= async function(passwordInputByUser) {
+ userSchema.methods.validatePassword= async function(passwordInputByUser) {
      const user=this;
      const passwordHash=user.password;
      const isPasswordValid=await bcrypt.compare(passwordInputByUser,passwordHash);
