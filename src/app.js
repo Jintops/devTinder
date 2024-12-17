a77f3749eb35a73cdd41d160aa3628c799654457
Jintops <jintops2003@gmail.com>
2024-12-18 00:25:21 +0530

    using expressjs router

--- src/app.js
+++ src/app.js
@@ -1,103 +1,45 @@
 const express = require('express');
 const connectDB = require('./config/database')
-const User = require("./models/user");
-const {validateSignUpDate}=require('./utils/validation')
-const bcrypt=require('bcrypt')
 const cookieParser=require('cookie-parser')
-const jwt=require('jsonwebtoken')
-const {userAuth}=require('./middlewares/auth');
-const { isPassportNumber } = require('validator');
+const authRouter=require('./routes/auth');
+const profileRouter=require('./routes/profile')
+const requestRouter=require('./routes/request')
+
+
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser())
 
-
-app.post("/signup",async (req,res)=>{
-  try{
-   const {firstName,lastName,emailId,password}=req.body;
-
-  validateSignUpDate(req)
-
-  const passwordHash=await bcrypt.hash(password,10)
-
-  const user = new User({
-  firstName,
-  lastName,
-  emailId,
-  password:passwordHash
-  })
- 
-  await user.save();
-  res.send("user data successfully saved")
- } catch(err){
-  res.status(400).send("ERROR :"+err.message)
- }
-}) ;
-
-app.post("/login",async (req,res)=>{
-  try{
-    const {emailId,password}=req.body;
-    const user=await User.findOne({emailId:emailId})
-    if(!user){
-      throw new Error("Invalid credential")
-    }
-    const isPasswordValid=await user.validatePassword(password)
-    if(isPasswordValid){
-
-      const token=await user.getJWT();
-      res.cookie("token",token, { expires: new Date(Date.now() + 900000)});
-      res.send("login success!!!!")
-    }else{
-      throw new Error("Invalid credential")
-    }
-  }catch(err){
-    res.status(400).send("ERROR :"+err.message)
-   }
-  
-})
-
-app.get("/profile", userAuth ,async(req,res)=>{
-  try{
-  
-  const user=req.user
-  res.send(user)
-  }catch(err){
-    res.status(400).send("ERROR :"+err.message)
-   }
-})
-
-app.post("/sendConnectionRequest",userAuth,async(req,res)=>{
-
-  console.log('sending connection request')
-   res.send('connection request sent')
-})
+app.use('/',authRouter);
+app.use('/',profileRouter);
+app.use('/',requestRouter)
 
 
 
 //update the document
-app.patch("/user/:userId",async(req,res)=>{ 
-  const userId=req.params?.userId;
-  const data= req.body
-  // console.log(emailId)
+// app.patch("/user/:userId",async(req,res)=>{ 
+//   const userId=req.params?.userId;
+//   const data= req.body
+//   // console.log(emailId)
 
-  try{
-    const ALLOWED_UPDATE = ["photoUrl", "gender", "age", "skills", "about"];
+//   try{
+//     const ALLOWED_UPDATE = ["photoUrl", "gender", "age", "skills", "about"];
 
-    // Check if all keys in the user object are allowed for update
-    const isUpdateAllowed = Object.keys(data).every((k) =>
-      ALLOWED_UPDATE.includes(k)
-    );
+//     // Check if all keys in the user object are allowed for update
+//     const isUpdateAllowed = Object.keys(data).every((k) =>
+//       ALLOWED_UPDATE.includes(k)
+//     );
 
-    if (!isUpdateAllowed) {
-      throw new Error("Update not allowed");
-    }
-      await User.findOneAndUpdate({userId:userId},data,{runValidators:true})
-      res.send("user updated successfully")
-  }catch(err){
-    res.status(400).send('issue in user update, '+err.message)
-  }
-})
+//     if (!isUpdateAllowed) {
+//       throw new Error("Update not allowed");
+//     }
+//       await User.findOneAndUpdate({userId:userId},data,{runValidators:true})
+//       res.send("user updated successfully")
+//   }catch(err){
+//     res.status(400).send('issue in user update, '+err.message)
+//   }
+// })
 
 connectDB().then(() => {
   console.log("database connected successfully....")
---
+++ src/routes/request.js
@@ -0,0 +1,13 @@
+const express =require('express');
+const requestRouter=express.Router();
+const {userAuth}=require('../middlewares/auth');
+
+
+requestRouter.post("/sendConnectionRequest",userAuth,async(req,res)=>{
+
+    console.log('sending connection request')
+     res.send('connection request sent')
+  });
+  
+
+module.exports=requestRouter;
