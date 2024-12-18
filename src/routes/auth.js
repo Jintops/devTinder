29c207d00450ea4b5fe7aea1d122a0e12a188300
Jintops <jintops2003@gmail.com>
2024-12-18 22:49:54 +0530

    logout api created

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -49,5 +49,12 @@ authRouter.post("/login",async (req,res)=>{
     
   })
 
+authRouter.post("/logout",async(req,res)=>{
+  res.cookie("token",null,{ 
+    expires: new Date(Date.now())
+  })
+  res.send("logout success!!!!")
+})
+
 
 module.exports=authRouter;
