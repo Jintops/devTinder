f8c34f419ab99b2c2e4928bc25f651868a1dc813
Jintops <jintops2003@gmail.com>
2025-07-01 00:47:13 +0530

    create payment api

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -1,29 +1,26 @@
-const express = require('express');
-const { userAuth } = require('../middlewares/auth');
+const express = require("express");
+const { userAuth } = require("../middlewares/auth");
 
-const paymentRouter=express.Router();
-const razorpayInstance=require("../utils/razorpay")
+const paymentRouter = express.Router();
+const razorpayInstance = require("../utils/razorpay");
 
+paymentRouter.post("/payment/create", userAuth, async (req, res) => {
+  try {
+    const order = await razorpayInstance.orders.create({
+      amount: 70000,
+      currency: "INR",
+      receipt: "receipt#1",
+      notes: {
+        firstName: "value3",
+        lastName: "value2",
+        membershipType: "silver",
+      },
+    });
 
-paymentRouter.post('/payment/create',userAuth,async(req,res)=>{
-    try{
-
-   await razorpayInstance.orders.create({
-  "amount": 50000,
-  "currency": "INR",
-  "receipt": "receipt#1",
-  "notes": {
-    "firstName": "value3",
-    "lastName": "value2"
+    res.json({order})
+  } catch (err) {
+    res.status(400).send("ERROR :" + err.message);
   }
-})
+});
 
-
-    }catch(err){
-        res.status(400).send("ERROR :" + err.message)
-    }
-})
-
-
-
-module.exports=paymentRouter;
+module.exports = paymentRouter;
--- src/utils/razorpay.js
+++ src/utils/razorpay.js
@@ -1,8 +1,9 @@
 const Razorpay=require('razorpay')
-console.log('Key ID:', process.env.RAZORPAY_KEY_ID);
+
 var instance = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID ,
   key_secret: process.env.RAZORPAY_KEY_SECRET
 });
+
 
 module.exports=instance;
