78b9ad7340c11059284548fb2cdd340cbf34f1f1
Jintops <jintops2003@gmail.com>
2025-07-01 03:12:10 +0530

    dynamic payment creation

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -3,21 +3,39 @@ const { userAuth } = require("../middlewares/auth");
 
 const paymentRouter = express.Router();
 const razorpayInstance = require("../utils/razorpay");
+const Payment=require('../models/payment')
+const membershipAmount=require("../utils/constants")
 
 paymentRouter.post("/payment/create", userAuth, async (req, res) => {
   try {
+    const {membershipType}=req.body;
+    const {firstName,lastName,emailId}=req.user;
+
     const order = await razorpayInstance.orders.create({
-      amount: 70000,
+      amount: membershipAmount(membershipType)*100,
       currency: "INR",
       receipt: "receipt#1",
       notes: {
-        firstName: "value3",
-        lastName: "value2",
-        membershipType: "silver",
+        firstName,
+        lastName,
+        emailId,
+        membershipType: membershipType,
       },
     });
 
-    res.json({order})
+    const payment=new Payment({
+        userId:req.user._id,
+        orderId:order.id,
+        status:order.status,
+        amount:order.amount,
+        currency:order.currency,
+        receipt:order.receipt,
+        notes:order.notes,
+    })
+
+    const savedPayment=await payment.save();
+
+    res.json({...savedPayment.toJSON()})
   } catch (err) {
     res.status(400).send("ERROR :" + err.message);
   }
