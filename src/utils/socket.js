384aa53b5c04d89ea51472b059bc9b33746d8437
Jintops <jintops2003@gmail.com>
2025-07-03 03:19:52 +0530

    send message

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -8,6 +8,25 @@ const initilizeSocket=(server)=>{
      })
  io.on("connection",(socket)=>{
 
+    socket.on("joinChat",({userId,targetUserId})=>{
+      
+      const roomId=[userId,targetUserId].sort().join("_")
+     console.log("joing roomh: "+roomId)
+
+     socket.join(roomId)
+    })
+     socket.on("sendMessage",({firstName,userId,targetUserId,text})=>{
+         const roomId=[userId,targetUserId].sort().join("_")
+         console.log(firstName +": "+ text)
+         io.to(roomId).emit("messageReceiver",{firstName,text})
+        
+    })
+
+
+     socket.on("disconnect",()=>{
+        
+    })
+
  })
 
 }
