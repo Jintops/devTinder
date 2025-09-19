158d89fde20c2c50456ede1de3a54ca5df559a87
Jintops <jintops2003@gmail.com>
2025-09-20 04:21:58 +0530

    error

--- src/app.js
+++ src/app.js
@@ -15,10 +15,13 @@ const chatRouter = require('./routes/chat');
 
 const app = express();
 app.use(cors({
-  origin:['http://localhost:5173','https://dev-tinder-frontend-sigma.vercel.app/'],
+  origin: [
+    "http://localhost:5173",
+    "https://dev-tinder-frontend-sigma.vercel.app"
+  ],
   credentials: true,
-
 }));
+
 app.use(express.json()); 
 app.use(cookieParser());
 
