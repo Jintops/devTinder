dad9f5b2880a89b5f1351c6fab01efd83c63b22c
Jintops <jintops2003@gmail.com>
2025-09-20 04:19:51 +0530

    update deployment

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ const chatRouter = require('./routes/chat');
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin:['http://localhost:5173','https://dev-tinder-frontend-sigma.vercel.app/'],
   credentials: true,
 
 }));
