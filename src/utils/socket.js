c87caf797c2a64a1c0befdc175990483b4f226c0
Jintops <jintops2003@gmail.com>
2025-09-20 16:14:44 +0530

    chat room connected to live

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -11,9 +11,10 @@ const getSecretRoomId = (userId, targetUserId) => {
 
 const initilizeSocket = (server) => {
   const io = socket(server, {
-    cors: {
-      origin: "http://localhost:5173",
-    },
+    origin: [
+        "http://localhost:5173",
+        "https://dev-tinder-frontend-sigma.vercel.app",
+      ],
   });
   io.on("connection", (socket) => {
     socket.on("joinChat", ({ userId, targetUserId }) => {
